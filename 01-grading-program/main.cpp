6b5eeb02e7acbdfe52d37d329a16f1ef7c313c08
t0mmie <thomas@angrybytes.com>
2022-08-19 15:51:41 +0200

    Add grading program

---
+++ 01-grading-program/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/* Write a program that allows the user to enter the grade scored in
+a programming class (0-100). If the user scored a 100 then notify
+the user that they got a perfect score.
+
+⭐ Modify the program so that if the user scored a 90-100 it informs
+the user that they scored an A
+
+⭐⭐ Modify the program so that it will notify the user of their letter grade
+0-59 F 60-69 D 70-79 C 80-89 B 90-100 A */
+
+int main() {
+    unsigned int grade;
+    std::string response;
+
+    std::cout << "Enter your grade: \n";
+
+    std::cin >> grade;
+
+    if (grade < 0 || grade > 100)
+        response = "That's not possible!";
+    else if (grade == 100)
+        response = "That's a perfect score!";
+    else
+        response = "You can do better.";
+
+    std::cout << response << "\n";
+
+    return 0;
+}
